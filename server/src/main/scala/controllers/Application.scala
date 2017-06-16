de6dfafcf4aeef6113b9402b692b89b03b435187
mariusz <wrotki@msn.com>
2017-06-16 16:57:42 -0700

    Commented out enough stuff to fail on scalajs

--- server/src/main/scala/controllers/Application.scala
+++ server/src/main/scala/controllers/Application.scala
@@ -6,8 +6,8 @@ import boopickle.Default._
 import com.google.inject.Inject
 import play.api.{Configuration, Environment}
 import play.api.mvc._
-import services.ApiService
-import spatutorial.shared.Api
+//import services.ApiService
+//import spatutorial.shared.Api
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -17,28 +17,28 @@ object Router extends autowire.Server[ByteBuffer, Pickler, Pickler] {
 }
 
 class Application @Inject() (implicit val config: Configuration, env: Environment) extends Controller {
-  val apiService = new ApiService()
+//  val apiService = new ApiService()
 
   def index = Action {
     Ok(views.html.index("SPA tutorial"))
   }
 
-  def autowireApi(path: String) = Action.async(parse.raw) {
-    implicit request =>
-      println(s"Request path: $path")
-
-      // get the request body as ByteString
-      val b = request.body.asBytes(parse.UNLIMITED).get
-
-      // call Autowire route
-      Router.route[Api](apiService)(
-        autowire.Core.Request(path.split("/"), Unpickle[Map[String, ByteBuffer]].fromBytes(b.asByteBuffer))
-      ).map(buffer => {
-        val data = Array.ofDim[Byte](buffer.remaining())
-        buffer.get(data)
-        Ok(data)
-      })
-  }
+//  def autowireApi(path: String) = Action.async(parse.raw) {
+//    implicit request =>
+//      println(s"Request path: $path")
+//
+//      // get the request body as ByteString
+//      val b = request.body.asBytes(parse.UNLIMITED).get
+//
+//      // call Autowire route
+//      Router.route[Api](apiService)(
+//        autowire.Core.Request(path.split("/"), Unpickle[Map[String, ByteBuffer]].fromBytes(b.asByteBuffer))
+//      ).map(buffer => {
+//        val data = Array.ofDim[Byte](buffer.remaining())
+//        buffer.get(data)
+//        Ok(data)
+//      })
+//  }
 
   def logging = Action(parse.anyContent) {
     implicit request =>
